1726fd25b321871657c060546e0b7075e9409e7f
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-04 21:38:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,10 @@
 # lib/introduction.rb
 
-def introduction(Jesse)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}".
 end
+name 
+language 
